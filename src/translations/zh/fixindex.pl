2ab9b874a7f739fc310847d7d32bdec5a98208d7
Israel Roldan <israel.alberto.rv@gmail.com>
2020-11-11 15:31:03 -0600

    Removed original source from the book.

--- src/translations/zh/fixindex.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-undef $/; 
-open(INDEX, "index.xml"); 
-$tmp = <INDEX>; 
-close(INDEX); 
-$tmp =~ s!-->.*?</indexentry>!-->!s; 
-open(INDEX, ">index.xml"); 
-print INDEX $tmp; 
-close(INDEX);
--- src/translations/zh/ramen.c
+++
@@ -1,5 +0,0 @@
-int printf(char *format, ...)
-{
-	write(1, "ILikeRamen!\n", 12);
-	return 0;
-}
