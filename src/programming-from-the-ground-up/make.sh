6f6236d8181ca389d11bb02a63cf793238776513
Israel Roldan <israel.alberto.rv@gmail.com>
2020-11-25 09:15:35 -0600

    Removed the footnotes parameter from the Markdown files.

--- src/programming-from-the-ground-up/make.sh
+++ src/programming-from-the-ground-up/make.sh
@@ -8,33 +8,33 @@ rm -f "${RELEASE_PATH}"ProgrammingGroundUp*
 rm -fR "${RELEASE_PATH}"resource
 cp -R resource "${RELEASE_PATH}"resource
 
-# Create the final ePUB version.
+# Create the final versions.
 pandoc -f markdown -t epub3 --standalone -o \
   "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}.epub" \
   001-ProgrammingGroundUp.txt \
   002-Book.md
 
-pandoc -f markdown -t markdown_github+footnotes --standalone -o \
+pandoc -f markdown -t markdown_github --standalone -o \
   "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}_github.md" \
   001-ProgrammingGroundUp.txt \
   002-Book.md
 
-pandoc -f markdown -t gfm+footnotes --standalone -o \
+pandoc -f markdown -t gfm --standalone -o \
   "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}_gfm.md" \
   001-ProgrammingGroundUp.txt \
   002-Book.md
 
-pandoc -f markdown -t commonmark+footnotes --standalone -o \
+pandoc -f markdown -t commonmark --standalone -o \
   "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}_commonmark.md" \
   001-ProgrammingGroundUp.txt \
   002-Book.md
 
-pandoc -f markdown -t markdown_strict+footnotes --standalone -o \
+pandoc -f markdown -t markdown_strict --standalone -o \
   "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}_markdown_strict.md" \
   001-ProgrammingGroundUp.txt \
   002-Book.md
 
-pandoc -f markdown -t html5 --standalone+footnotes --highlight-style pygments -o \
+pandoc -f markdown -t html5 --standalone --highlight-style pygments -o \
   "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}.html" \
   001-ProgrammingGroundUp.txt \
   002-Book.md
