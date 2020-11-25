8189230ac7e9359dee050150a17fcb3525376fd7
Israel Roldan <israel.alberto.rv@gmail.com>
2020-11-25 00:46:54 -0600

    Added the release folder into the script make; Added the releases files.

--- src/programming-from-the-ground-up/make.sh
+++ src/programming-from-the-ground-up/make.sh
@@ -1,6 +1,26 @@
 #!/bin/bash -ev
 
+RELEASE_PATH="../../release/"
+VERSION="v"$(date "+%Y.%m.%d")
+
+mkdir -p "${RELEASE_PATH}"
+rm -f "${RELEASE_PATH}ProgrammingGroundUp_*"
+rm -fR "${RELEASE_PATH}resource"
+cp -R "resource" "${RELEASE_PATH}resource"
+
 # Create the final ePUB version.
-pandoc -o ProgrammingGroundUp.epub \
+pandoc -f markdown -t epub3 -o "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}.epub" \
+  001-ProgrammingGroundUp.txt \
+  002-Book.md
+
+pandoc -f markdown -t markdown_github -o "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}.md" \
+  001-ProgrammingGroundUp.txt \
+  002-Book.md
+
+pandoc -f markdown -t html5 -o "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}.html" \
+  001-ProgrammingGroundUp.txt \
+  002-Book.md
+
+pandoc -f markdown -t pdf -o "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}.pdf" \
   001-ProgrammingGroundUp.txt \
   002-Book.md
