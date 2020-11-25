39c8890b9ec36a5fd5f8d071b894bf639f1041f6
Israel Roldan <israel.alberto.rv@gmail.com>
2020-11-25 17:30:27 -0600

    Added Lua filter to include files to the code block.

--- src/programming-from-the-ground-up/make.sh
+++ src/programming-from-the-ground-up/make.sh
@@ -3,38 +3,52 @@
 RELEASE_PATH="../../release/"
 VERSION="v"$(date "+%Y.%m.%d")
 
+
 mkdir -p "${RELEASE_PATH}"
 rm -f "${RELEASE_PATH}"ProgrammingGroundUp*
 rm -fR "${RELEASE_PATH}"resource
 cp -R resource "${RELEASE_PATH}"resource
 
+
 # Create the release versions.
-pandoc -f markdown -t epub3 --standalone --highlight-style pygments -o \
-  "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}.epub" \
+pandoc -f markdown -t epub3 --standalone --highlight-style pygments \
+  --lua-filter \
+    resource/pandoc/lua/include-code-files.lua \
+  -o "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}.epub" \
   001-ProgrammingGroundUp.txt \
   002-Book.md
 
-pandoc -f markdown -t markdown_strict --standalone --highlight-style pygments -o \
-  "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}_markdown_strict.md" \
+pandoc -f markdown -t markdown_strict --standalone --highlight-style pygments \
+  --lua-filter \
+    resource/pandoc/lua/include-code-files.lua \
+  -o "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}_markdown_strict.md" \
   001-ProgrammingGroundUp.txt \
   002-Book.md
 
-pandoc -f markdown -t commonmark --standalone --highlight-style pygments -o \
-  "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}_commonmark.md" \
+pandoc -f markdown -t commonmark --standalone --highlight-style pygments \
+  --lua-filter \
+    resource/pandoc/lua/include-code-files.lua \
+  -o "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}_commonmark.md" \
   001-ProgrammingGroundUp.txt \
   002-Book.md
 
-pandoc -f markdown -t gfm --standalone --highlight-style pygments -o \
-  "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}_github.md" \
+pandoc -f markdown -t gfm --standalone --highlight-style pygments \
+  --lua-filter \
+    resource/pandoc/lua/include-code-files.lua \
+  -o "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}_github.md" \
   001-ProgrammingGroundUp.txt \
   002-Book.md
 
-pandoc -f markdown -t html5 --standalone --highlight-style pygments -o \
-  "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}.html" \
+pandoc -f markdown -t html5 --standalone --highlight-style pygments \
+  --lua-filter \
+    resource/pandoc/lua/include-code-files.lua \
+  -o "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}.html" \
   001-ProgrammingGroundUp.txt \
   002-Book.md
 
-pandoc -f markdown -t pdf --standalone --highlight-style pygments -o \
-  "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}.pdf" \
+pandoc -f markdown -t pdf --standalone --highlight-style pygments \
+  --lua-filter \
+    resource/pandoc/lua/include-code-files.lua \
+  -o "${RELEASE_PATH}ProgrammingGroundUp_${VERSION}.pdf" \
   001-ProgrammingGroundUp.txt \
   002-Book.md
